9944fabf749a463e916a80ee7ef2d34d962ca915
Anagh Padmanabhan <anagh.parambath@gmail.com>
2016-12-21 16:57:03 +0530

    Updated README. Added functionalities for caching.

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ All publisher websites should be cached to reduce load to the server.
 * Update the package in [Packagist](https://packagist.org/).
 * To use the new version of the package in any project, change the version number in composer.json file and run
 `$ composer update `
+
+###Implementation
+Pass the required argument(array) to function **buildCacheHeaders()**, and pass the return value(array) as the argument to Laravel function **withHeaders()** on **response()**.
+The argument is an associative array with following keys:
+1. publisherId
+2. cdnTTLs: Corresponds to Surrogate-Control values.
+..1. max-age: This controls how long the page is considered fresh in the Database.
+..2. stale-while-revalidate: During this period, the page is served from CDN, but updated in the background.
+..3. stale-if-error: During this time, the page is served from CDN in case the backend server crashes for whatever reason.
+3. browserTTLs: Corresponds to Cache-Control values
+..1. max-age
+4. locationId(not required for story page): home or section-id
+5. storyGroup(not required for story page): top or stack-id
+6. storiesToCache: Array of all the stories that have to be cached.
---
+++ src/Quintype/Caching.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Quintype\Caching;
+
+class Caching
+{
+    private function buildStoryKeys($params)
+    {
+        $storyKeys = [];
+        foreach ($params['storiesToCache'] as $story) {
+            array_push($storyKeys, 'ss/'.$params['publisherId'].'/'.substr(trim($story['id']), 0, 8));
+        }
+
+        return implode(' ', $storyKeys);
+    }
+
+    private function buildSurrogateKey($cacheParams)
+    {
+        $surrogateKey = '';
+        if (isset($cacheParams['locationId']) && isset($cacheParams['storyGroup'])) {
+            $surrogateKey = 'sq/'.$cacheParams['publisherId'].'/'.$cacheParams['storyGroup'].'/'.$cacheParams['locationId'];
+        }
+
+        if (isset($cacheParams['storiesToCache'])) {
+            $surrogateKey = $surrogateKey.' '.$this->buildStoryKeys($cacheParams);
+        }
+
+        return ['Surrogate-Key' => $surrogateKey];
+    }
+
+    public function buildCacheHeaders($cacheParams)
+    {
+      print_r($cacheParams);
+        $browserTTLs = $cacheParams['browserTTLs'];
+        $cdnTTLs = $cacheParams['cdnTTLs'];
+
+        $commonHeaders = [
+          'Cache-Control' => 'public,max-age='.$browserTTLs['max-age'],
+          'Surrogate-Control' => 'public,max-age='.$cdnTTLs['max-age'].',stale-while-revalidate='.$cdnTTLs['stale-while-revalidate'].',stale-if-error='.$cdnTTLs['stale-if-error'],
+          'Vary' => 'Accept-Encoding',
+        ];
+
+        return array_merge($commonHeaders, $this->buildSurrogateKey($cacheParams));
+    }
+}
